2af57722a7677b847c42a883f851d00a247a3139
Eric Elliott <support@paralleldrive.com>
2022-12-28 21:41:39 -0800

    Randogram

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import { createId } from "../src/index";
 
-const RandomDistribution = ({ width = 250, height = width}) => {
+const RandomDistribution = ({ width = 250, height = width }) => {
   const canvasRef = useRef(null);
 
   useEffect(() => {
@@ -13,8 +13,12 @@ const RandomDistribution = ({ width = 250, height = width}) => {
       const id = createId().substring(1);
 
       // Convert the id to a number between 0 and 1
-      const xValue = Number(parseInt(id.substring(0, 10), 36) / Math.pow(36, 10));
-      const yValue = Number(parseInt(id.substring(11, 21), 36) / Math.pow(36, 10));
+      const xValue = Number(
+        parseInt(id.substring(0, 10), 36) / Math.pow(36, 10)
+      );
+      const yValue = Number(
+        parseInt(id.substring(11, 21), 36) / Math.pow(36, 10)
+      );
 
       // Plot a pixel at a position determined by the id
       const x = Math.floor(xValue * width);
